8dd297a029bf820fe6d917ceb4ce01d9317cb538
hamkkong <akhenkitera@gmail.com>
2023-06-21 23:43:20 +0900

    chapter05전체 등록

---
+++ src/chapter05/src/chapter05/Exam08_Dowhile.java
@@ -0,0 +1,26 @@
+package chapter05;
+
+public class Exam08_Dowhile {
+
+    public static void main(String[] args) {
+	
+	int num = 0;
+	int count = 0;
+	
+	do {
+	    
+	    num++;
+	    
+	    if ((num % 5) != 0)
+		continue;
+	    if ((num % 7) != 0)
+		continue;
+	    
+	    count++;                    //5와 7의 배수인 경우에만 실행 
+	    System.out.println(num);    //5와 7의 배수인 경우에만 실행 
+	} while(num < 100);
+
+	System.out.println("count:" + count);
+    }
+
+}
