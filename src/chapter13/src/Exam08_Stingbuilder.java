bd657b1a20102db33b95dc6b2add3383fb994b16
hamkkong <akhenkitera@gmail.com>
2023-06-29 18:23:29 +0900

    스트링빌더 예졔

---
+++ src/chapter13/src/Exam08_Stingbuilder.java
@@ -0,0 +1,25 @@
+
+public class Exam08_Stingbuilder {
+
+    public static void main(String[] args) {
+	StringBuilder buf = new StringBuilder("동해물과");
+	
+	buf.append("백두산이");        //추가 
+	System.out.println(buf.toString());   //형변환 후 출력 
+	
+	buf.append(12345);          // 추가 
+	System.out.println(buf.toString());
+	
+	buf.delete(0, 4);        // 구간 삭제 
+	System.out.println(buf.toString());
+	
+	buf.replace(4, 8 , "ABC");  // 값 변경 
+	System.out.println(buf.toString());
+	
+	buf.reverse();             // 순서 반전 
+	System.out.println(buf.toString());
+	
+
+    }
+
+}
