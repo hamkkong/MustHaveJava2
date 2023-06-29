d98fd8ea52cb28d9d7cf682e1c0d9b9050c18e1e
hamkkong <akhenkitera@gmail.com>
2023-06-29 18:50:26 +0900

    문자열 분할(토큰)예제

---
+++ src/chapter13/src/Exam09_StringTokenizer.java
@@ -0,0 +1,16 @@
+import java.util.StringTokenizer;
+
+public class Exam09_StringTokenizer {
+
+    public static void main(String[] args) {
+
+	StringTokenizer st1 = new StringTokenizer("a b c"); // 공백 기준 분
+	// StringTokenizer st1 = new StringTokenizer("1,2,3", ",");
+
+	while (st1.hasMoreTokens()) { // 토큰 유무 확인
+
+	    System.out.println(st1.nextToken());
+	}
+    }
+
+}
