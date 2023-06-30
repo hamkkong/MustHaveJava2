76dcb60b949ff1f4588a67d123c848d80e0db296
hamkkong <akhenkitera@gmail.com>
2023-06-30 18:14:00 +0900

    매개변수로 들어오는 배열의 값(이클립스)

---
+++ src/chapter14/src/Exam07_MainParameter.java
@@ -0,0 +1,12 @@
+
+public class Exam07_MainParameter {
+
+    public static void main(String[] args) {
+	
+	// main() 메서드의 매개변수 확인 
+	for (int i = 0; i < args.length; i++)
+	    System.out.println(args[i]);
+	
+    }
+
+}
