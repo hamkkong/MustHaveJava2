9ee0f26d1a7fcc43e35e4caf55a79e1b2f644e10
hamkkong <akhenkitera@gmail.com>
2023-07-01 18:41:21 +0900

    예외 살펴보기

---
+++ src/chapter15/src/Exam01_ExceptionCase.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Exam01_ExceptionCase {
+
+    public static void main(String[] args) {
+	
+	Scanner sc = new Scanner(System.in);
+	int num1 = sc.nextInt();
+	int num2 = 10 / num1;
+	System.out.println(num2);
+	
+	// Mybook book1 = new Mybook();
+
+
+    }
+
+}
