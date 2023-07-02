0b81610a4cac171947c74f2d5512fe7883b435c1
hamkkong <akhenkitera@gmail.com>
2023-07-02 22:05:03 +0900

    toString을 사용한 예제

---
+++ src/chapter16/src/Exam02_toString1.java
@@ -0,0 +1,11 @@
+public class Exam02_toString1 {
+
+    public static void main(String[] args) {
+	
+	String name = "홍길동";
+	System.out.println(name);
+	System.out.println(name.toString());
+
+    }
+
+}
