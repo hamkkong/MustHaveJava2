819e49880f978542dd5a570277dbb00d514b0b8d
hamkkong <akhenkitera@gmail.com>
2023-07-03 12:03:02 +0900

    래퍼 클래스

---
+++ src/chapter16/src/Exam07_Number.java
@@ -0,0 +1,16 @@
+public class Exam07_Number {
+
+    public static void main(String[] args) {
+	
+	//Integer num1 = new Integer(20);
+	Integer num1 = Integer.valueOf(20);
+	System.out.println(num1.intValue());
+	System.out.println(num1.doubleValue());
+	
+	Double num2 = Double.valueOf(3.14);
+	System.out.println(num2.intValue());
+	System.out.println(num2.doubleValue());
+
+    }
+
+}
