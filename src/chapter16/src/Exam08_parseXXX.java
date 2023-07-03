e1f56b360268f207158605e1663f8b0359c77cf0
hamkkong <akhenkitera@gmail.com>
2023-07-03 12:23:13 +0900

    문자열을 수치형으로 형변환

---
+++ src/chapter16/src/Exam08_parseXXX.java
@@ -0,0 +1,13 @@
+public class Exam08_parseXXX {
+
+    public static void main(String[] args) {
+	
+	String str = "100";
+	int a = Integer.parseInt(str);
+	double b = Double.parseDouble("3.14");
+	
+	System.out.println(a + " : " + b);
+
+    }
+
+}
