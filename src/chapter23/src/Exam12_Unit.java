ea79984b26898c1eaebe09ab7d9ac76e258cd727
hamkkong <akhenkitera@gmail.com>
2023-07-09 22:34:01 +0900

    객체 저장 직렬화 클래스 생성

---
+++ src/chapter23/src/Exam12_Unit.java
@@ -0,0 +1,14 @@
+public class Exam12_Unit implements java.io.Serializable {
+    private static final long serialVersionUID = 1L;
+    private String name;
+    
+    public Exam12_Unit(String name) {
+	
+	this.name = name;
+    }
+    
+    public String getname() {
+	
+	return name;
+    }
+}
