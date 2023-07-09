6fce77ef1cd71884313b78490db3775b72f77418
hamkkong <akhenkitera@gmail.com>
2023-07-09 13:25:40 +0900

    파일 대상 입력 스트림 생성

--- src/chapter23/src/Exam03_FileWrite3.java
+++ src/chapter23/src/Exam03_FileWrite3.java
@@ -1,8 +1,18 @@
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 
 public class Exam03_FileWrite3 {
 
     public static void main(String[] args) {
-	// TODO Auto-generated method stub
+	try (OutputStream out = new FileOutputStream("data.txt")) {
+	    
+	    out.write(65);  //ASXII 코드 65 = 'A'
+	}
+	catch(IOException e) {
+	    
+	    e.printStackTrace();
+	}
 
     }
 
