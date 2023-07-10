e64a5548c84898b962436148a02b45c7f0632922
hamkkong <akhenkitera@gmail.com>
2023-07-10 14:41:57 +0900

    스레드에서의 문제점

---
+++ src/chapter24/src/Exam06_ProblemOfThread.java
@@ -0,0 +1,32 @@
+public class Exam06_ProblemOfThread {
+
+    public static int money = 0;
+  
+    public static void main(String[] args)throws InterruptedException  {
+	
+	Runnable task1 = () -> {
+	    for (int i = 0; i<10000; i++)
+		money++;
+	};
+	
+	Runnable task2 = () -> {
+	    for (int i = 0; i<10000; i++)
+		money--;
+	};
+	
+	Thread t1 = new Thread(task1);
+	Thread t2 = new Thread(task2);
+	
+	t1.start();
+	t2.start();
+	
+	t1.join();  //t1이 참조하는 스레드의 종료를 기다림
+	t2.join();  //t2가 참조하는 스레드의 종료를 기다림
+	
+	//스레드가 종료되면 출력을 진행함. 위 join의 영향 
+	System.out.println(money);
+	
+
+    }
+
+}
