e1e3e22990b22bf7ef419dc7fee0c07090f9aa7e
hamkkong <akhenkitera@gmail.com>
2023-08-14 18:24:10 +0900

    8월 12일 예제

--- src/example Study/src/streamEx/File01.java
+++ src/example Study/src/streamEx/File01.java
@@ -6,7 +6,7 @@ public class File01 { //C 드라이브를 열어주고 .list() 를 이용해서 
 
     public static void main(String[] args) {
 	//new File("새로 만들거나 제어하고 싶은 파일이나 폴더의 경로")
-	File f = new File("c://Users/ihyemi/Documents/workspace/mustHaveJava2/MustHaveJava/src/address\\ book ");
+	File f = new File("c:/");
 	String[] names = f.list();
 	// 배열에 있는 내용을 반복문 돌면서 콘솔창에 모두 출력할 수 있을까?
 	
